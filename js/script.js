ed9380227a0b992c3b0b86e55ee2bbfe7c5d83ac
Software Developer <i4uprovider@gmail.com>
2018-08-17 23:56:12 +0300

    add mobile dropdown menu

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ jQuery(document).ready(function($) {
 	$(".go-bottom a").on("click", function() {
 		$("html, body").animate({ scrollTop: $('#whatwedo').offset().top }, 1000);
 	});
+
+	$(".dropdownico").on("click", function() {
+		if( $(this).parent().parent().find("li").first().hasClass("responsive") )
+			$(this).parent().parent().find("li").removeClass("responsive");
+		else
+			$(this).parent().parent().find("li").addClass("responsive");
+	});
 })
