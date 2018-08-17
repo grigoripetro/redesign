0228c762cbdbc562c4ef1b6d895ae65008899e1f
Software Developer <i4uprovider@gmail.com>
2018-08-17 03:33:55 +0300

    updated step 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 /* Custom Javascript */
 jQuery(document).ready(function($) {
-
+	$(".go-bottom a").on("click", function() {
+		$("html, body").animate({ scrollTop: $('#whatwedo').offset().top }, 1000);
+	});
 })
